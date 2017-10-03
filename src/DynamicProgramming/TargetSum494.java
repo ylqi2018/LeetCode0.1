176bcffb24d886ba2355291594b7eae85851c356
ylqi007 <yunlongqi@my.unt.edu>
2017-10-03 00:24:23 -0500

    Dynamic, 494 TargetSum

---
+++ src/DynamicProgramming/TargetSum494.java
@@ -0,0 +1,30 @@
+package DynamicProgramming;
+
+public class TargetSum494 {
+
+	public int findTargetSumWays(int[] nums, int s) {
+		int sum = 0;
+		for(int n : nums) {
+			sum += n;
+		}
+		
+		return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s+sum)>>>1);
+	}
+	
+	public int subsetSum(int[] nums, int s) {
+		int[] dp = new int[s + 1];
+		dp[0] = 1;
+		for(int n : nums) {
+			for(int i=s; i>=n; i--) {
+				dp[i] += dp[i-n];
+			}
+		}
+		return dp[s];
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
