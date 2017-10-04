842cee8ec369053435594599cba8e14c43b8aaca
ylqi007 <yunlongqi@my.unt.edu>
2017-10-04 00:10:53 -0500

    494-Version_0.1

--- src/DynamicProgramming/TargetSum494.java
+++ src/DynamicProgramming/TargetSum494.java
@@ -21,6 +21,26 @@ public class TargetSum494 {
 		}
 		return dp[s];
 	}
+
+	
+	int count = 0;
+	public int findTargetSumWays1(int[] nums, int S) {
+		calculate(nums, 0, 0, S);
+		return count;
+	}
+	private void calculate(int[] nums, int i, int sum, int S) {
+		if(i == nums.length) {
+			if(sum == S) {
+				count++;
+			}
+		} else {
+			calculate(nums, i+1, sum+nums[i], S);
+			calculate(nums, i+1, sum-nums[i], S);
+		}
+	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
