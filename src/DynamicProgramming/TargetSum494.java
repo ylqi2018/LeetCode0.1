e7201d601915dd5f8e32d6a29c6c3350f96cfc35
ylqi007 <yunlongqi@my.unt.edu>
2017-10-04 11:07:24 -0500

    494-Version0.2

--- src/DynamicProgramming/TargetSum494.java
+++ src/DynamicProgramming/TargetSum494.java
@@ -75,7 +75,24 @@ public class TargetSum494 {
 		}
 	}
 	
-	
+	/**
+	 * 
+	 * @param args
+	 */
+	public int findTargetSumWays3(int[] nums, int S) {
+		int[][] dp = new int[nums.length][2001];
+		dp[0][nums[0]+1000] = 1;
+		dp[0][-nums[0]+1000] += 1;	// For here must be +=, in the case of nums[0]==0
+		for(int i=1; i<nums.length; i++) {
+			for(int sum=-1000; sum<=1000; sum++) {
+				if(dp[i-1][sum+1000]>0) {
+					dp[i][sum+1000+nums[i]] += dp[i-1][sum+1000];
+					dp[i][sum+1000-nums[i]] += dp[i-1][sum+1000];
+				}
+			}
+		}
+		return S > 1000 ? 0 : dp[nums.length-1][S+1000];
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
